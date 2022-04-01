bd9eba04832132b4a9d87a119f4f9701e29c7bbb
Samuel Mandel <slman.760@gmail.com>
2022-03-31 18:53:23 -0700

    add accessibility to the prompt,alert, and confirm method; add array storage variables for those methods except prompt number for pass length

--- script.js
+++ script.js
@@ -16,44 +16,72 @@ generateBtn.addEventListener("click", writePassword);
 
 function  generatePassword() {
     var passwordArray = []
-    //numnum takes userinput for password length
-    var numnum = prompt("Enter desired password length (8-128 characters):")
-    confirm("please pick one of the following types")
-    var lowCase = confirm();
-    var upCase = confirm();
-    var num = confirm();
-    var specialChar = confirm(); 
+    var passwordLength = prompt("Enter desired password length (8-128 characters):")
+    var passwordLength = parseInt(passwordLength, 10);
 
-    if ((lowCase === true) || 
+    if (passwordLength <= 128 && passwordLength >= 8) {
+        var typeConfirm = confirm("please pick one of the following types")
+        
+        if( typeConfirm === false) {
+            var areYouSure = confirm('Do you want a password?');
+            if (areYouSure === true) {
+                generatePassword();
+            }
+        }
+
+    }
+    else{
+        var confirmError =confirm("you must pick a number between 8-128");
+        if (confirmError === true) {
+            generatePassword();
+        }
+        else {
+            alert("no password for you")
+        }
+    }
+
+    if(typeConfirm ===true) {
+        var lowCase = confirm("Hit ok for lower case letters");
+        var upCase = confirm("Hit ok for upper case letters");
+        var num = confirm("Hit ok for numbers");
+        var specialChar = confirm("Hit ok for special characters"); 
+
+        if ((lowCase === true) || 
         (upCase === true) || 
         (num === true) || 
         (specialChar === true) ) {
             if (lowCase===true){
-                var lC = ['a b c d e f g h i j k l o m n o p q r s w x y z'];
-                var lCArray = lC.split(' ')
-                passwordArray.concat(lC);
+                var lC = 'a b c d e f g h i j k l o m n o p q r s w x y z';
+                var lCArray = lC.split(' ');
+                var passwordArray = passwordArray.concat(lCArray);
             }
             if (upCase === true) {
                 var uC = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z';
                 var uCArray = uC.split(" ");
-                console.log(uCArray);
                 var passwordArray = passwordArray.concat(uCArray);
-                console.log(passwordArray)
             }
             if (num === true) {
                 var num = '0 1 2 3 4 5 6 7 8 9';
-                passwordArray.concat(num);
+                var numArray = num.split(' ');
+                var passwordArray = passwordArray.concat(numArray);
             }
             if (specialChar === true) {
                 var specialChar =  "! \" # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _ ` { | } ~";
                 var specialCharArray = specialChar.split(" ");
                 var passwordArray = passwordArray.concat(specialCharArray);
-                passwordArray.concat(specialChar);
-            }
+            } 
         }
-    else {
-        alert("you must pick one type")
-        generatePassword();
-    }
+        else {
+            confirmType=confirm("you must pick one type");
+            if(confirmType === true)
+            {
+                generatePassword();
+            }
+            else {
+                alert('no password for you :(');
+            }
+            
+        }
+    }    
     console.log(passwordArray);
  }
